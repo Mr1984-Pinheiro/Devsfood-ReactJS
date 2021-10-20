91125358e65fb42971fcf4d623f71b3ed2f48330
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2021-10-20 02:56:51 -0300

    Inicia criar Header da pagina Home

---
+++ src/components/Header/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background-color: #FA8072;
+    border-radius: 10px;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+
+`;
+
+export const Logo = styled.img`
+width: auto;
+height: 70px;
+`;
+
+export const SearchInput = styled.input`
+
+`;
