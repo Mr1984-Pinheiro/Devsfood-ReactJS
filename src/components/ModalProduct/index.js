3d6e37b8406826eef978b1de414c29850f1c5970
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-01-21 01:25:00 -0300

    Modal Product CartReducer Part 1

--- src/components/ModalProduct/index.js
+++ src/components/ModalProduct/index.js
@@ -38,6 +38,12 @@ export default ({ data, setStatus }) => {
         setQt(qt + 1);
     }
 
+    const handleAddToCart = () => {
+
+
+        setStatus(false);
+    }
+
     return (
         <Container>
             <ProductArea>
@@ -61,7 +67,7 @@ export default ({ data, setStatus }) => {
             </ProductArea>
             <ProductButtons>
                 <ProductButton small={true} onClick={handleCancelButton} >Cancelar</ProductButton>
-                <ProductButton>Adicionar ao Carrinho</ProductButton>
+                <ProductButton onClick={handleAddToCart} >Adicionar ao Carrinho</ProductButton>
             </ProductButtons>
         </Container>
     );
---
+++ src/reducers/CartReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+    products: [],
+    address: [],
+    discount: 0,
+    delivery: 0
+
+};
+
+export default (state = initialState, action) => {
+    // eslint-disable-next-line
+    switch (action.type) {
+        /*case 'SET_TOKEN':
+            return { ...state, token: action.payload.token };
+            break;
+        case 'SET_NAME':
+            return { ...state, name: action.payload.name };
+            // eslint-disable-next-line
+            break;*/
+    }
+
+    return state;
+}
